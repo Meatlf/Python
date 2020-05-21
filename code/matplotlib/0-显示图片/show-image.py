a67bac994a32b60d117dfd4ed12f5c3a8efc1f24
Meatlf <454779758@qq.com>
2020-05-21 13:54:37 +0800

    feat 解决了显示彩色图出现的色差问题，增加了README.md

--- code/matplotlib/0-显示图片/show-image.py
+++ code/matplotlib/0-显示图片/show-image.py
@@ -3,7 +3,11 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import numpy as np
  
-src = cv.imread("1.png")
+src0 = cv.imread("1.png")
+plt.imshow(src0)
+plt.show()
 
-plt.imshow(src)
+src1 = cv.imread("2.jpg")
+src1RGB = cv.cvtColor(src1,cv.COLOR_BGR2RGB)
+plt.imshow(src1RGB)
 plt.show()
