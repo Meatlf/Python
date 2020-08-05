602282399d8dcaf5bf46814cbbfd26c48c57a0a3
Meatlf <454779758@qq.com>
2020-08-05 14:51:12 +0800

    feat 增加了求重心的功能

--- opencv-python/Chapter14-轮廓/FindContoursV2.py
+++ opencv-python/Chapter14-轮廓/FindContoursV2.py
@@ -2,6 +2,7 @@
 # 参考资料
 # [1] [Python-Opencv 轮廓常用操作](https://www.cnblogs.com/zhangxiaoman/p/12390742.html)
 # [2] [Python+OpenCV图像处理之轮廓发现](https://www.cnblogs.com/qianxia/p/11102800.html)
+# [3] [OpenCV—轮廓操作一站式详解：查找/筛选/绘制/形状描述与重心标注(Python版)](https://blog.csdn.net/iracer/article/details/90695914#7.%E6%A0%87%E6%B3%A8%E8%BD%AE%E5%BB%93%E9%87%8D%E5%BF%83)
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -26,10 +27,17 @@ for i, contour in enumerate(contours):
     if((cv2.pointPolygonTest(contour, pt, False) == 1) and (cv2.contourArea(contour) < minArea)):
         minArea = cv2.contourArea(contour)
         I=i
-img = cv2.drawContours(ROI, contours, I, (0, 255, 0), 1)
+ROI = cv2.drawContours(ROI, contours, I, (0, 255, 0), 1)
 
-blue = (255, 0, 0)
-ROI = cv2.circle(ROI, pt, 1, blue, -1)
+red = (255, 0, 0)
+blue = (0,0,255)
+green = (0,255,0)
+
+ROI = cv2.circle(ROI, pt, 1, red, -1)  # 画红点
+
+mom = cv2.moments(contours[I])
+pt = (int(mom['m10'] / mom['m00']), int(mom['m01'] / mom['m00']))  # 使用前三个矩m00, m01和m10计算重心
+cv2.circle(ROI, pt, 1, blue, -1)  # 画蓝点
 
 plt.imshow(ROI)
 plt.show()
