1964d627cad24322c5a3b6e964ed8b378d4c789c
Meatlf <454779758@qq.com>
2020-08-05 14:09:37 +0800

    feat 实现了在ROI中查找轮廓并进行筛选的功能

--- opencv-python/Chapter14-轮廓/FindContoursV2.py
+++ opencv-python/Chapter14-轮廓/FindContoursV2.py
@@ -1,4 +1,4 @@
-# 在ROI中查找轮廓
+# 在ROI中查找轮廓并进行筛选
 # 参考资料
 # [1] [Python-Opencv 轮廓常用操作](https://www.cnblogs.com/zhangxiaoman/p/12390742.html)
 # [2] [Python+OpenCV图像处理之轮廓发现](https://www.cnblogs.com/qianxia/p/11102800.html)
@@ -10,38 +10,26 @@ import math
 img = cv2.imread('FinderPattern.bmp')
 
 pt = (15, 25)
-# img = img[pt[1]-10:pt[1]+10, pt[0]-10:pt[0]+10]
+ROI = img[pt[1]-13:pt[1]+13, pt[0]-13:pt[0]+13]
 
-imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+ROIgray = cv2.cvtColor(ROI, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(ROIgray, 127, 255, 0)
 contours, hierarchy = cv2.findContours(
-    thresh[pt[1]-12:pt[1]+12, pt[0]-12:pt[0]+12], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-
-# 遍历所有轮廓
-pt = (15-12, 25-12)
-minArea = 100
+# 遍历所有轮廓r
+pt = (13, 13)
+minArea = 1000
 I = 0
-# for i, contour in enumerate(contours):
-#     if (cv2.pointPolygonTest(contour, pt, False) == 1 and cv2.contourArea(contour) < minArea):
-#         minArea = cv2.contourArea(contour)
-#         I = i
-# img = cv2.drawContours(img, contours, I, (0, 255, 0), 1)
 
 for i, contour in enumerate(contours):
-    # if (cv2.pointPolygonTest(contour, pt, False) == 1 and cv2.contourArea(contour) < minArea):
-    #     minArea = cv2.contourArea(contour)
-    I = i
-    img = cv2.drawContours(img, contours, I, (0, 255, 0), 1)
-
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+    if((cv2.pointPolygonTest(contour, pt, False) == 1) and (cv2.contourArea(contour) < minArea)):
+        minArea = cv2.contourArea(contour)
+        I=i
+img = cv2.drawContours(ROI, contours, I, (0, 255, 0), 1)
 
 blue = (255, 0, 0)
-pt = (15, 25)
-img = cv2.circle(img, pt, 1, blue, -1)
+ROI = cv2.circle(ROI, pt, 1, blue, -1)
 
-# isInPolygon = cv2.pointPolygonTest(contours[0], pt, False)
-# print(isInPolygon)
-plt.imshow(img)
+plt.imshow(ROI)
 plt.show()
