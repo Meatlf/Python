e050a4188440139ec865ddce7bab50bd11d799a7
Meatlf <454779758@qq.com>
2020-08-18 17:19:20 +0800

    feat 布雷森汉姆直线算法,霍夫变换,python for循环

--- opencv-python/Chapter14-轮廓/FindContoursV1.py
+++ opencv-python/Chapter14-轮廓/FindContoursV1.py
@@ -1,6 +1,8 @@
+# 功能：查找轮廓，该算法不需要轮廓筛选
 # 参考资料
 # [1] [Python-Opencv 轮廓常用操作](https://www.cnblogs.com/zhangxiaoman/p/12390742.html)
 # [2] [Python+OpenCV图像处理之轮廓发现](https://www.cnblogs.com/qianxia/p/11102800.html)
+# [3] [opencv进行反色操作](https://blog.csdn.net/qq_42444944/article/details/98473476)
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
@@ -10,8 +12,9 @@ img = cv2.imread('FinderPattern.bmp')
 
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+cv2.bitwise_not(thresh, thresh)
 contours, hierarchy = cv2.findContours(
-    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 pt = (15, 25)
 # 遍历所有轮廓
@@ -20,13 +23,10 @@ for i, contour in enumerate(contours):
         print(i)
         img = cv2.drawContours(img, contours, i, (0, 255, 0), 1)
 
-# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-
-# blue = (255, 0, 0)
-# img = cv2.circle(img, pt, 1, blue, -1)
-
-# isInPolygon = cv2.pointPolygonTest(contours[0], pt, False)
-# print(isInPolygon)
+plt.figure()
+plt.subplot(1,2,1)
+thresh = cv2.cvtColor(thresh,cv2.COLOR_BGR2RGB)
+plt.imshow(thresh)
+plt.subplot(1,2,2)
 plt.imshow(img)
 plt.show()
--- python/for.py
+++ python/for.py
@@ -1,3 +1,6 @@
 # for循环
 for i in range(1, 20):
     print(i)
+
+for i in range(3):
+    print(i)
